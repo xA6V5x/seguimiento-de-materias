cd1522ec4269ec03cb306cd55d861ad92cf62881
xA6V5x <ojedaagustinmanuel@gmail.com>
2025-08-03 03:22:55 -0300

    update editarMateriaCorrelativas()

--- admin/src/editarMateriaCorrelativas.c
+++ admin/src/editarMateriaCorrelativas.c
@@ -7,5 +7,53 @@
 
 void editarMateriaCorrelativas(materia_archivo_t *materia)
 {
+    int opcion;
+
+    int contadorOpcion = 1;
+
+    materias_t materias = leerBinDeMaterias();
+
+    int estadoId = 0;
+
+    int materiasLength = materias.length;
+
+    materia_archivo_t *materiasArray = materias.array;
+    do
+    {
+        system("cls");
+
+        printf("== SELECCIONAR CORRELATIVAS ==\n");
+        printf("0 - Cancelar\n");
+        printf("-------------------------------\n");
+
+        if (materia->correlativasLength > 0)
+        {
+            for (int i = 0; i < materia->correlativasLength; i++)
+            {
+                printf("%d - %d\n", contadorOpcion, (materiasArray[i].correlativas));
+                contadorOpcion++;
+            }
+            printf("-------------------------------\n");
+        }
+
+        // No tener en cuenta las materias que actualmente son correlativas
+        if ((materiasLength - materia->correlativasLength) == 0)
+        {
+            printf("\n**NO SE ENCONTRARON MAS MATERIAS**\n");
+        }
+        else
+        {
+            for (int i = 0; i < materiasLength; i++)
+            {
+                printf("%d - %s\n", contadorOpcion, (materiasArray[i].nombre));
+                contadorOpcion++;
+            }
+        }
+
+        printf("\nSeleccione una opcion: ");
+        scanf("%d", &opcion);
+        limpiarBuffer();
+    } while (opcion != 0);
+
     return;
 }
